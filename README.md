bc8d0c62f3167462d073ae7ef877ea24536c5f71
iworldtong <iworldtong@gmail.com>
2019-12-25 14:20:48 +0800

    remove cuda

--- README.md
+++ README.md
@@ -4,21 +4,21 @@ This project implements ["Graph Convolutional Networks for Text Classification. 
 
 This implementation highly based on official code [yao8839836/text_gcn](<https://github.com/yao8839836/text_gcn>).
 
-# Require
+## Require
 
 * Python 3.6
 * PyTorch 1.0
 
-# Running training and evaluation
+## Running training and evaluation
 
 1. `cd ./preprocess`
 2. Run `python remove_words.py <dataset>`
 3. Run `python build_graph.py <dataset>`
-4. `cd ../`
+4. `cd ..`
 5. Run `python train.py <dataset>`
 6. Replace `<dataset>` with `20ng`, `R8`, `R52`, `ohsumed` or `mr`
 
-# Visualization
+## Visualization
 
 ### R8
 
--- train.py
+++ train.py
@@ -78,16 +78,16 @@ t_support = []
 for i in range(len(support)):
     t_support.append(torch.Tensor(support[i]))
 
-if torch.cuda.is_available():
-    model_func = model_func.cuda()
-    t_features = t_features.cuda()
-    t_y_train = t_y_train.cuda()
-    t_y_val = t_y_val.cuda()
-    t_y_test = t_y_test.cuda()
-    t_train_mask = t_train_mask.cuda()
-    tm_train_mask = tm_train_mask.cuda()
-    for i in range(len(support)):
-        t_support = [t.cuda() for t in t_support if True]
+# if torch.cuda.is_available():
+#     model_func = model_func.cuda()
+#     t_features = t_features.cuda()
+#     t_y_train = t_y_train.cuda()
+#     t_y_val = t_y_val.cuda()
+#     t_y_test = t_y_test.cuda()
+#     t_train_mask = t_train_mask.cuda()
+#     tm_train_mask = tm_train_mask.cuda()
+#     for i in range(len(support)):
+#         t_support = [t.cuda() for t in t_support if True]
         
 model = model_func(input_dim=features.shape[0], support=t_support, num_classes=y_train.shape[1])
 
